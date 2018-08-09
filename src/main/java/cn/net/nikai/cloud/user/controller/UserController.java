bdf1f0272696d451cc839686efc1968aeab43d2d
nikai <keni1006@163.com>
2018-08-10 00:12:05 +0800

    update

--- src/main/java/cn/net/nikai/cloud/user/controller/UserController.java
+++ src/main/java/cn/net/nikai/cloud/user/controller/UserController.java
@@ -8,7 +8,6 @@ import com.jiatui.spi.util.JsonFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +19,8 @@ import java.util.Map;
  * @author nikai
  * @version 1.0.0
  */
-@RestController("/ai")
+@RestController
+@RequestMapping("/ai")
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
